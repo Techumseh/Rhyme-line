f36f2e8e9443bf9fd802831da4ad8ea2b7ed9af5
Jeff Bond <jeffbond@MacBook-Pro.local>
2020-12-10 14:02:59 -0500

    small changes to appearance

--- rhyme-line-app/src/components/Header.jsx
+++ rhyme-line-app/src/components/Header.jsx
@@ -5,7 +5,8 @@ import logo from "../assets/rhyme-line.png"
  function Header() {
    return (
      <header>
-       <img src={logo }></img>
+       <img src={logo}></img><br></br>
+       <h3>   bringing you the finest playlist creation tools since 12.2020</h3>
       </header>
    );
 }
